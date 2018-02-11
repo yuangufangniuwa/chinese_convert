fca94f1fa250b6a8129626c70331582e8d5bf550
yuangufangniuwa <36382090+yuangufangniuwa@users.noreply.github.com>
2018-02-12 04:18:33 +0800

    Update test.js

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var convert = require('../chinese_convert')
 
 describe('convert', function () {
   it('test cn2tw', function () {
-    assert.equal(convert.cn2tw('简体转繁体功能测试'), '簡體轉繁體功能測試')
+    assert.equal(convert.cn2tw('简体转繁体功能测试'), '體轉繁體功能測試')
   })
   it('test tw2cn', function () {
     assert.equal(convert.tw2cn('繁體轉簡體功能測試'), '繁体转简体功能测试')
